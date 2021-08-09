caf9b8aba8dc989ef04443f2ccd95227cb1dcbfb
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-08-09 16:39:11 -0400

    Add files via upload

---
+++ school.js
@@ -0,0 +1,51 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('school.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status != "") {
+        for(var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("object").innerHTML = "There are 3 objects, out of which " + objects.length + " is/are detected.";
+
+            percent = floor(objects[i].confidence * 100);
+            fill("#FF0000");
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function backHome() {
+    window.location = "index.html";
+}
+
+function modelLoaded() {
+    console.log("Model loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+} 
